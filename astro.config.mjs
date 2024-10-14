85bdc6c9f5f98508c9a247b46c1c1fc091c2b7dc
memo92975 <memo92975@gmail.com>
2024-10-14 00:12:54 +0000

    Elimina el plugin de mdx porque no lo necesito

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    markdown : {
-        shikiConfig : {
-            theme : "material-theme-ocean"
-        },
-    }
+  markdown : {
+      shikiConfig : {
+          theme : "material-theme-ocean"
+      },
+  },
+
+  integrations: []
 });
